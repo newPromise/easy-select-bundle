de1b700033dab95470619a4bfd237a44b7653d57
zhangningning <zhangningning@powerlaw.ai>
2020-11-22 20:32:21 +0800

    electron 按需加载

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui';
+import { Checkbox } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-// import { Checkbox } from 'element-ui'
 import App from './App.vue'
 
-// Vue.use(Checkbox)
+Vue.use(Checkbox)
 
-Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
